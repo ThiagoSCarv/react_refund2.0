ac6a9efeac4b4f46d28405a6a7c69e308d71fa7f
Thiago <thiagocarv4@gmail.com>
2026-04-29 21:52:50 -0300

    feat: pagina NewRefund

--- src/pages/NewRefund.tsx
+++ src/pages/NewRefund.tsx
@@ -1,7 +1,20 @@
+import { NewRefundForm } from "../contexts/refunds/components/NewRefundForm";
+
 export function NewRefund() {
 	return (
-		<main>
-			<h1>New Refund</h1>
+		<main className="flex items-center justify-center p-10">
+			<div className="bg-white rounded-2xl p-10 w-full max-w-lg flex flex-col gap-8">
+				<div className="flex flex-col gap-1">
+					<h1 className="text-2xl font-bold text-gray-100">
+						Nova solicitação de reembolso
+					</h1>
+					<p className="text-sm text-gray-200">
+						Dados da despesa para solicitar reembolso.
+					</p>
+				</div>
+
+				<NewRefundForm />
+			</div>
 		</main>
 	);
 }
